ba3e4159cbda92bcc53ecd33cb1e554aa97e6fea
Omkar443 <sahniomkar218@gmail.com>
2026-02-26 10:15:22 +0530

    Updated the now and launch time

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -174,9 +174,9 @@ const CountdownTimer: React.FC<CountdownTimerProps> = () => {
           {timeLeft.hours}h {timeLeft.minutes}m {timeLeft.seconds}s until launch
         </div>
         <div className="text-xs text-amber-500/40 font-mono mt-1 flex gap-2">
-          <span>Now: 8:47 AM</span>
+          {/* <span>Now: 8:47 AM</span> */}
           <span>•</span>
-          <span>Launch: 6:30 PM</span>
+          {/* <span>Launch: 6:30 PM</span> */}
         </div>
       </motion.div>
     </div>
